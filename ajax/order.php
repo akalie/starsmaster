f2d3910535a6d8e3ac7aebe1cda17f2809b2993e
akalie <askaslie@gmail.com>
2014-04-10 12:54:01 +0400

    Let there be light

---
+++ ajax/order.php
@@ -0,0 +1,17 @@
+<?php
+/* Sites-Stroy.ru by iProger */
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$adminEmail = 'a.krylov@topface.com';
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers .= 'To: '.$adminEmail . "\r\n";
+$headers .= 'From: StarMaster <noreply@sites-stroy.ru>' . "\r\n";
+
+$mess = "Новый заказ StarMaster. <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel;
+
+mail($adminEmail, "Новый заказ", $mess, $headers);
+
+?>
