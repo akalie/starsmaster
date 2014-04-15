d9abaeb729db303f694726f24637a09d665fe234
freez <evgeeek@gmail.com>
2014-04-15 12:32:57 +0400

    Тест смены мыла отправки и целевого мыла

--- ajax/order.php
+++ ajax/order.php
@@ -3,12 +3,12 @@
 $fio = $_POST['fio'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
-$adminEmail = 'a.krylov@topface.com';
+$adminEmail = 'evgeeek@gmail.com';
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 $headers .= 'To: '.$adminEmail . "\r\n";
-$headers .= 'From: StarMaster <noreply@sites-stroy.ru>' . "\r\n";
+$headers .= 'From: StarMaster <noreply@starsmaster.net>' . "\r\n";
 
 $mess = "Новый заказ StarMaster. <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel;
 
