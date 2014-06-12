9fe4eafba75ff54d00a8fefdee4dc0b759cfe358
akalie <askaslie@gmail.com>
2014-06-12 21:33:39 +0400

    again

---
+++ ajax/order.php
@@ -0,0 +1,32 @@
+<?php
+/* Sites-Stroy.ru by iProger */
+
+function SendMail($em_to,$subject,$mess)
+{
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'To: '.$em_to . "\r\n";
+	$headers .= 'From: StarMaster <noreply@starsmaster.net>' . "\r\n";
+	
+	mail($em_to, $subject, $mess, $headers);
+}
+
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$adress = $_POST['adress'];
+
+$adminEmail = 'starsmaster@allsocial.ru';
+
+$mess = "Новый заказ StarMaster. <br/>ФИО: ".$fio."<br/>"."E-mail: ".$email."<br/>"."<br/>Телефон: ".$tel."<br/>Адрес: ".$adress;
+
+SendMail($adminEmail, 'Новый заказ StarMaster', $mess);
+
+$mess = <<<HDO
+Уважаемый $fio <br/>
+Вы заказали у нас на starsmaster.net ночник-проектор, указали адрес $adress, в течение рабочего дня с Вами свяжется наш менеджер для подтверждения заказа.<br/>
+Возникшие вопросы вы можете задать по номеру телефона +7(909)588-64-58.<br/>
+Спасибо, что воспользовались нашими услугами!
+HDO;
+
+SendMail($email, 'Ваш заказ', $mess);
---
+++ index.php
@@ -0,0 +1,359 @@
+<?php header ("Content-Type: text/html; charset=utf-8"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- Sites-Stroy.ru by iProger -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Ночник - проектор звездного неба Star Master</title>
+	<link media="all" rel="stylesheet" type="text/css" href="css/style.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/jquery.countdown.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/gallery.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="js/jquery.countdown.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+    <LINK rel="icon" href="images/favicon.png" type="image/x-icon">
+</head>
+<body>
+	<div class=wrapper>
+		<div class=header>
+			<div class=main>
+				<div class=htext>
+					<img src="images/logo.png" /><br/>
+					Популярный хит сезона — ночник-проектор настоящего,<br/>
+					натурального звездного неба "Star-Master" по специальной<br/>
+					самой низкой цене в интернете!
+				</div>
+				<div class=himg><img src="images/starmaster.png" /></div>
+				<div class=block-exclusive>
+					<div class=ex-head></div>
+					<div class=ex-body>
+						<div class=stock-price></div>
+						<small>Заполните пожалуйста поля</small>
+						<form class=sval-form method=POST action="ajax/order.php" target="stars">
+						<input name=fio type=text sval="ФИО" value="ФИО"/>
+						<input name=email type=text sval="E-mail" value="E-mail"/>
+						<input id="tel" name=tel type=text sval="+7(___) ___-__-__" value="+7(___) ___-__-__"/>
+						<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/>
+                            <button class="buy">КУПИТЬ</button>
+						</form>
+						<br/>
+					</div>
+				</div><br/><br/><br/><br/><br/><br/>
+				</div>
+			</div>
+		</div>
+		<div class=stock-time>
+			<div class=main>
+				<div class=banner>
+					<img src="images/banner-time.png" /><br/>
+					<span>Осталось 17 светильников со скидкой</span>
+					<!-- http://ruseller.com/lessons.php?rub=32&id=1341  -->
+				</div>
+				<div class=timer>
+					<div id="countdown"></div>
+					<div class="days">Дни</div>
+					<div class="hours">Часы</div>
+					<div class="mins">Минуты</div>
+					<div class="secs">Секунды</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=features>
+			<div class=main>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-1.png"/></div>
+					<div class=ftext>
+						<h3>Два режима свечения и новое свечение на каждый месяц</h3>
+						Режим звезд, обычного светильника, а также 12 свечений на каждый месяц подарят массу эмоций
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-2.png"/></div>
+					<div class=ftext>
+						<h3>До 20 дней работы от батареек или USB</h3>
+						Подключайте обычные батарейки и берите StarMaster с собой в походы и путешествия. Подключайте к USB, чтобы забыть о батарейках
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-3.png"/></div>
+					<div class=ftext>
+						<h3>Безопасен для детей</h3>
+						<br/>
+						Невозможно обжечься. Светильник StarMaster полностью безопасен для детей
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=about>
+			<div class=main>
+				<h1>Что такое StarMaster?</h1>
+				<div class=video>
+					<iframe src="http://vk.com/video_ext.php?oid=246376291&id=168072329&hash=8f68dc8cfcaa9f15&hd=1" width="449" height="264" frameborder="0"></iframe>
+				</div>
+				<div class=info>
+					Чтобы увидеть звезды не обязательно посещать планетарий, астрономическую обсерваторию, 
+					или выходить ночью на неосвещенную фонарями улицу или выезжать, чтобы увидеть чистое небо, з
+					а город. Ночник-проектор позволяет любоваться звездным не выходя из дома! Звезды будут сверкать 
+					на потолке и стенах комнаты, как настоящие, вызывая бурный восторг у детей и взрослых.
+					<div class=gallery style="margin-bottom:20px;">
+						<div class=gallery_container>
+							<img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift0.jpg" class='gallery_main animation in'>
+						</div>
+						<div class=gallery_thumbnails>
+							<div class=gallery_thumbnail><img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift0.jpg"></div>
+							<div class=gallery_thumbnail><img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift00.jpg"></div>
+							<div class=gallery_thumbnail><img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift1.jpg"></div>
+							<div class=gallery_thumbnail><img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift2.jpg"></div>
+							<div class=gallery_thumbnail><img src="http://img2.apishops.org/SinglePageWebsites/custom/images/starmaster.widescreen/gift3.jpg"></div>
+						</div>				
+					</div>
+				</div>
+				<div class=clear></div><br/>
+				<center><a href="#buy"><button class=bynow id="buy2">Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=benefits>
+			<div class=main>
+				<h1>Преимущества StarMaster:</h1>
+				<div class=center>
+					<ul style="margin-right:180px;">
+						<li>Незабываемое звездное ночное шоу для детей и взрослых</li>
+						<li>Украшение для любого интерьера детской или гостинной</li>
+						<li>Атмосфера глубокого и успокающевого космоса</li>
+					</ul>
+					<ul>
+						<li>Шоу, которым можно делиться в путешествиях со своими друзьями</li>
+						<li>Звездное небо, которое не увидеть в крупных городах, прямо у Вас дома</li>
+						<li>Радость каждому ребенку, который хоть раз интересовался звездами</li>
+					</ul>
+					<div class=clear></div>
+					<div class=rarrows></div>
+					<img src="images/ok.png" align="left" />
+					<div class=oktext>
+						<h2>StarMaster — отличный подарок себе и близким</h2>
+						Если вы мечтаете засыпать под открытым небом, находясь при этом<br/>
+						в теплой постели, хотите создать сказочную атмосферу, расположить<br/>
+						гостя к доверительной беседе или ваш малыш грезит о звездах,<br/> 
+						то StarMaster — подходящий подарок!
+					</div>
+					<div class=clear></div>
+ 				</div>
+				<br/>
+				<center><a href="#buy" id="buy3"><button class=bynow>Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=reviews>
+			<div class=main>
+				<h1>Отзывы наших счастливых клиентов</h1>
+				<div class=review>
+					<div class=avatar><img src="images/ava-1.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Артем Митрюхин</strong><br/>
+							<small>г. Смоленск</small>
+						</div>
+						Друзья пригласили нас на крещение своего третьего ребенка. Нужно было что-то подарить, мне пришла идея подарить этот<br/>
+						проектор, такой подарок сразу для всех детей! А зная родителей, я был уверен, что и они будут рады
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-2.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Комолов</strong><br/>
+							<small>г. Калининград</small>
+						</div>
+						Увидев рекламу в интернете, очень захотелось приобрести проектор звездного неба Star Master У друзей есть такое чудо техники.<br/>
+						Купил она его недавно и еще толком не проверил. Пока радуюсь только от эффекта звездного неба
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-3.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Олег Ежков</strong><br/>
+							<small>г. Казань</small>
+						</div>
+						Купил сие чудо в начале осени. Мне очень нравится, во первых он работает от батареек и довольно таки экономично.<br/>
+                        Занимает мало места, но когда включаешь его, то вся комната освещена.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div id=hide-rev style="display:none;">
+				<div class=review>
+					<div class=avatar><img src="images/ava-4.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Стерхов</strong><br/>
+							<small>г. Подольск </small>
+						</div>
+						Все красиво, и проектор и упаковка.Спасибо! И правда, звезды очень отчетливо видны!
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-5.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Лилия Ильина</strong><br/>
+							<small>г. Тобольск</small>
+						</div>
+						Заказала в довесок к большому заказу другой техники. Привезли очень быстро через 2 дня, хотя доставке не по Москве, в регион, не близко.
+                        Очень аккуратнинько всё упаковано. Всё как написано в описании, не прибавить не убавить. Не стоит ожидать сверх предложенного. Замечательный светильничек.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-6.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Павел Андреев</strong><br/>
+							<small>г. Казань</small>
+						</div>
+                       До Казани посылка дошла за 8 дней. Интересный проектор. В целом своих денег стоит.
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-7.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Леонид Дмитриев</strong><br/>
+							<small>г. Брянск</small>
+						</div>
+                         Мобильный и можно подключать через USB. Мне очень понравился. Рекомендую!
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-8.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Стегнов</strong><br/>
+							<small>г. Сочи</small>
+						</div>
+                       Получил светильник за короткий промежуток, всего 9 дней. Подарил своему племяннику, он в восторге. Спасибо!
+					</div>
+					<div class=clear></div>
+				</div>
+				</div>
+				<span onclick="showRev(this);" style="cursor:pointer; color:#049fbd;">Показать ещё >></span>
+			</div>
+		</div>
+		<div class=our-benefits>
+			<div class=main>
+				<h1>Наши преимущества</h1>
+				<div class="center2">
+				<div class=oben>
+					<img src="images/garant.png" />
+					<span class=obname>100% гарантия возврата денег.</span>
+					<div class=obdesc>Товар устроит вас или мы вернем вам деньги!</div>
+				</div>
+				<div class=oben>
+					<img src="images/bmap.png" />
+					<span class=obname>Доставка по всей России</span>
+					<div class=obdesc>Доставка во все города России.
+					Доставка курьером и пункты
+					самовывоза по всей Москве.</div>
+				</div>
+				<div class=oben style="margin-right:0px;">
+					<img src="images/econom.png" />
+					<span class=obname>Низкие цены</span>
+					<div class=obdesc>Мы работаем напрямую с производителем,
+					поэтому можем позволить себе акции и
+					низкие цены.</div>
+				</div>
+				<div class=clear></div>
+				</div>
+			</div>
+		</div>
+		<div class=howjob>
+			<div class=main>
+				<h1>Как мы работаем</h1>
+				<center>
+					<div class=steps>
+						<div class=step1>
+							<span>Вы оставляете заявку</span><br/>
+							Вы оставляете заявку на сайте или по телефону
+						</div>
+						<div class=step2>
+							<span>Мы доставляем Вашу посылку</span><br/>
+							Мы быстро доставляем Вашу посылку по указанному адресу
+						</div>
+						<div class=step3>
+							<span>Наш менеджер связывается с вами</span><br/>
+							Менеджер связывается с Вами для <br/>подтверждения заказа
+						</div>
+						<div class=step4>
+							<span>Вы получаете свой товар</span><br/>
+							Вы получаете свой товар и платите <br/>за него на почте или курьеру
+						</div>
+					</div>
+				</center>
+			</div>
+		</div>
+		<div id=buy class=footer>
+		    <div class=main>
+				<center>
+				<h1>Купи <span style="codlor:red;">StarMaster</span> сегодня!</h1><br/><br/>	
+				<form class=sval-form method=POST action="ajax/order.php" target="stars">
+				<input name=fio type=text sval="ФИО" value="ФИО" />
+				<input name=email type=text sval="E-mail" value="E-mail"/>
+				<input id="tel2" name=tel type=text sval="+7(___) ___-__-__" value="+7(___) ___-__-__"/>
+				<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/><br/>
+				<button class="buy">Купить сейчас</button>
+				</form>
+				</center>
+			</div>
+		</div>
+		<center class=copy>ООО "КУРЬЕР" ОГРН: 1137746208049 ИНН: 7725785302 КПП: 772501001 ОКПО: 17277510 Юр. Адрес: 115280, г. Москва, Автозаводская ул., дом 17, корп. 3, офис 11</center>
+	</div>
+	<div id="suc_order" class="modal_window"> 
+		<div class="close"></div>
+		<p>Мы отправили на ваш e-mail письмо с данными заказа. Если оно долго не приходит - попробуйте проверить папку "Спам".</p>
+	</div>
+	<div id="scroller" class="b-top" style="display: none;"><span class="b-top-but"></span></div>
+   <div class="metrics" style="display: none">
+        <a href="https://metrika.yandex.ru/stat/?id=24618374&amp;from=informer"
+           target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/24618374/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                               style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:24618374,lang:'ru'});return false}catch(e){}"/></a>
+        <!-- /Yandex.Metrika informer -->
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter24618374 = new Ya.Metrika({id:24618374,
+                            webvisor:true,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true});
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="//mc.yandex.ru/watch/24618374" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+   </div>
+</body>
+</html>
